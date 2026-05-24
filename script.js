b67267ff3615a648a2f40c4ad0ac2ae3295ef1ac
venom24L <godzillatheking916@gmail.com>
2026-05-24 15:47:01 +0300

    Add script.js starter file

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript starter file
+console.log('Welcome to My App!');
+
+// Add your JavaScript code here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM is fully loaded and parsed');
+});
